da9ed641ff27ea5f3163ea4eff71a6c1e979edff
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2022-03-10 17:18:08 +0200

    created env variables for aws user access key id and secret access key

--- api/sns.py
+++ api/sns.py
@@ -1,17 +1,24 @@
+import os
 
 import boto3
 from botocore.exceptions import ClientError
 
-sns_resource = boto3.resource('sns')
+IAM_USER_ACCESS_KEY_ID = os.environ['IAM_USER_ACCESS_KEY_ID']
+IAM_USER_SECRET_ACCESS_KEY = os.environ['IAM_USER_SECRET_ACCESS_KEY']
+
+sns_resource = boto3.resource('sns',
+                              aws_access_key_id=IAM_USER_ACCESS_KEY_ID, 
+                              aws_secret_access_key=IAM_USER_SECRET_ACCESS_KEY)
 
 def publish_text_message(phone_number, message):
     try:
         response = sns_resource.meta.client.publish(
             PhoneNumber=phone_number, Message=message)
         message_id = response['MessageId']
+        status_code = response['ResponseMetadata']['HTTPStatusCode']
         
     except ClientError:
         raise
     
     else:
-        return message_id
+        return message_id, status_code
